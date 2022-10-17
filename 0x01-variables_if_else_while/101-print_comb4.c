a61e0d5e64283ca97a46bd46a1895d44c2dd882e
vpnchengo <charo.alvin@gmail.com>
2022-10-17 02:11:39 -0700

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * random combinations of three digits
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 48 ; a <= 57 ; a++)
+	{
+		for (b = 49 ; b <= 57 ; b++)
+		{
+			for (c = 50 ; c <= 57 ; c++)
+			{
+				if (c > b && b > a)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 55 || b != 56)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
