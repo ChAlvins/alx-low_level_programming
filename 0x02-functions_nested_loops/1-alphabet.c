81cf6133d64641e0d1c56d1ac0ac0ce90a76b81f
vpnchengo <charo.alvin@gmail.com>
2022-10-18 04:13:01 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - in lowercase
+ *
+ *
+ */
+
+void print_alphabet(void)
+{
+	char d = 'a';
+
+	while (d <= 'z')
+	{
+		_putchar(d);
+		d++;
+	}
+	_putchar('\n');
+}
