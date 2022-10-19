b65c47fea39c08b69dbd8c9b8bce5f9f9961c2aa
vpnchengo <charo.alvin@gmail.com>
2022-10-19 02:05:09 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,33 +6,32 @@
  */
 void times_table(void)
 {
-	int a;
-	int b;
-	int c;
+	int a, b, c;
 
-	for (a = '0' ; a <= '9'; a++)
+	for (a = 0 ; a <= 9; a++)
 	{
-		_putchar(0);
-	{
-		for (b = '0' ; b <= '9'; b++)
+		for (b = 0 ; b <= 9; b++)
 		{
-			_putchar(',');
-			_putchar(' ');
-
 			c = a * b;
-
-			if (c <= 9)
+			if (b == 0)
 			{
-				_putchar(c);
-				else
-				{
-					_putchar((c / 10) + '0');
-					_putchar((c % 10) + '0');
-				}
+				_putchar(c + 0);
 			}
-			_putchar('\n');
+			else if (c < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
 		}
+	_putchar('\n');
 	}
 }
-
-
