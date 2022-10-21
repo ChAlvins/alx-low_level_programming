44082a8b2ae648ad6be0525b16b8213d487e3b18
vpnchengo <charo.alvin@gmail.com>
2022-10-21 04:17:43 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - drawing a straight line in the terminal
+ * @n: number of times the character _ is printed
+ */
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a <= n; a++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
