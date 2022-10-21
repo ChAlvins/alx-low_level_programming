e2e77b0f1d8445307236d995dd9e5a9d8fbb2ac1
vpnchengo <charo.alvin@gmail.com>
2022-10-21 02:46:09 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - printing 10 times the numbers, from 0 to 14,
+ * followed by a new line
+ *
+ */
+void more_numbers(void)
+{
+	int a = 0, b;
+
+	while (a <= 10)
+	{
+		for (b = 0; b <= 14; b++)
+			_putchar(b + '0');
+	}
+	_putchar('\n');
+}
+
