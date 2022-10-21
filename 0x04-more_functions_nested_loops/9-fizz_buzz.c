c1dababb33c850f0e0c7a17fa04f0f3d83d7316f
vpnchengo <charo.alvin@gmail.com>
2022-10-21 06:52:03 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,19 +16,23 @@ int main(void);
 	{
 		if (x % 15 == 0)
 		{
-			printf(" FizzBuzz");
+			printf("FizzBuzz");
 		}
 		else if (x % 5 == 0)
 		{
-			printf(" Buzz");
+			printf("Buzz");
 		}
 		else if (x % 3 == 0)
 		{
-			printf(" Fizz");
+			printf("Fizz");
 		}
 		else
 		{
-			printf(" %d", x);
+			printf("%d", x);
+		}
+		if (x < 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
