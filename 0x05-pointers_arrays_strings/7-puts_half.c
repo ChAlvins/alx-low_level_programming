4bb231ce0f0734e81df87476abab1045f910c330
vpnchengo <charo.alvin@gmail.com>
2022-10-25 07:25:34 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - printing half of a string, followed by a new line
+ * @str: string
+ *
+ *
+ */
+
+void puts_half(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		a++;
+		for (a /= 2; str[a] != 0; a++)
+		{
+			_putchar(str[a]);
+		}
+	}
+	_putchar('\n');
+}
