af23d354f9ac1dabb4d7f177b8fc969f0421f6b6
vpnchengo <charo.alvin@gmail.com>
2022-10-25 01:21:00 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int -  swaping the values of two integers
+ * @a: integer one
+ * @b: integer two
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
