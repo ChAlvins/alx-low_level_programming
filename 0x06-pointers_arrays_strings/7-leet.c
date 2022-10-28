e6eb6388e629c484b069f6d6bc4b0bf5b1586ac5
vpnchengo <charo.alvin@gmail.com>
2022-10-28 12:08:07 -0700

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @a: string
+ * Return: string
+ */
+
+char *leet(char *a)
+{
+	int i;
+	int j;
+	char b[] = "aAeEoOtTlL";
+	char c[] = "4433007711";
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (j = 0 && j <= 9; j++;)
+		{
+			if (a[i] == b[j])
+
+				a[i] = c[j];
+		}
+	}
+	return (a);
+}
