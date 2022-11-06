1243563b94877b102f9fb1c241208e3b16fe1dda
vpnchengo <charo.alvin@gmail.com>
2022-11-06 07:10:13 -0800

    update

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,5 +8,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int check_sqrt_recursion(int i, int j);
+
 
 #endif /* MAIN_H */
