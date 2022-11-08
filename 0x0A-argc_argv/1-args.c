557b73eb937801caae864b2cdf7d9c0639085f85
vpnchengo <charo.alvin@gmail.com>
2022-11-08 02:23:30 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: argument count
+ * @argv: array of argument
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)*argv;
+	printf("argc = %d\n", argc - 1);
+	return (0);
+}
