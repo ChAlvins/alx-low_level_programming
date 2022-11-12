a27388ef08e86809abe1ddb15a02e0691b776b8e
vpnchengo <charo.alvin@gmail.com>
2022-11-12 01:54:05 -0800

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,7 +5,7 @@
  * malloc_checked - function that allocates memory using malloc
  * @b: unsigned integer
  * Return: a pointer to the allocated memory
- * if malloc fails, the malloc_checked function 
+ * if malloc fails, the malloc_checked function
  * should cause normal process termination with a status value of 98
  */
 
