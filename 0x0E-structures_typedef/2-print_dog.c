0e09631fd707cfc30fc5eff37f44d951228bfb56
vpnchengo <charo.alvin@gmail.com>
2022-11-15 12:09:55 -0800

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,7 +19,7 @@ void print_dog(struct dog *d)
 	}
 	if (d->owner == NULL)
 	{
-		printf("(nil)\n");
+		printf("Owner: (nil)\n");
 	}
-	printf("Name:%s\nAge:%f\nOwner:%s\n", d->name, d->age, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
