04df824b2016863803022db7698bcf9db697eacd
vpnchengo <charo.alvin@gmail.com>
2022-11-27 22:32:42 -0800

    update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,21 @@
 #define FILE_MAIN_H
 #include <stdlib.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+}
+
 int _putchar(char c);
 
 #endif
