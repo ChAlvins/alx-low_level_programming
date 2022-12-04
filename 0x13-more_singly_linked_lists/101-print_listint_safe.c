5607a1f2e1480044788379cd38485a8d0e0d0989
vpnchengo <charo.alvin@gmail.com>
2022-12-04 12:13:29 -0800

    task 12

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint_safe - function that prints a listint_t linked list
+ * @head: pointer head of linked list
+ * Description: Go through the list only once.
+ * Return: number of nodes in list
+ * If fails,exit with status 98.
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *tmp;
+	size_t k;
+	const listint_t *rem;
+
+	tmp = head;
+	if (tmp == NULL)
+	{
+		exit(98);
+	}
+	k = 0;
+	while (tmp != NULL)
+	{
+		rem = tmp;
+		tmp = tmp->next;
+		k++;
+		printf("[%p] %d\n", (void *)rem, rem->n);
+		if (rem < tmp)
+		{
+			printf("-> [%p] %d\n", (void *)tmp, tmp->n);
+			break;
+		}
+	}
+	return (k);
+}
