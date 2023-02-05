1aaf43e8685d5d41c513ee8f74ce0dea037d8086
vpnchengo <charo.alvin@gmail.com>
2023-02-05 19:31:27 +0000

    task4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: pointer to hash table
+ * @key: the key to get the value
+ * Return:the value asociated with the element
+ * or NULL if key couldnt be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *temp;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+	index = hash_djb2((const unsigned char *)key) % ht->size;
+	temp = ht->array[index];
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+	return (NULL);
+}
