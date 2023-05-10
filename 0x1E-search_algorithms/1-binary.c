1623585a0a156b379ba4f432185d105af743aed1
vpnchengo <charo.alvin@gmail.com>
2023-05-10 19:57:29 +0000

    binary search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,33 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - linear search algorithm for arrays.
+ * @array: A pointer to inputed array.
+ * @size: The size of array.
+ * @value: The value to search for.
+ * Return: The index of the value into the array.
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i, l, r;
+
+	if (!array || size <= 0)
+		return (-1);
+
+	for (l = 0, r = size - 1; r >= l;)
+	{
+		printf("Searching in array: ");
+		for (i = l; i < r; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		i = l + (r - l) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			r = i - 1;
+		else
+			l = i + 1;
+	}
+	return (-1);
+}
